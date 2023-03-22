121bcac9be1079f3553c6dfecf31cdbffe030245
Yuliia Kolomiiets <jk182013@gmail.com>
2023-03-22 18:31:48 +0100

    feat: Mise en place du content

--- src/component/layout/Content.js
+++ src/component/layout/Content.js
@@ -1,3 +1,20 @@
+import styles from './Content.module.scss';
+
 export default function Content(){
-    return <div className="flex-fill">Content</div>
+    return <div className="flex-fill container">
+        <h1 className="p-20">Decouvrez nos nouvelles recettes</h1>
+        <div className={`card p-20 mb-20 ${styles.contentCard}`}>
+            <div className={styles.grid}>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                <div className={styles.element}>ELEMENT</div>
+                
+            </div>
+        </div>
+    </div>
 }
